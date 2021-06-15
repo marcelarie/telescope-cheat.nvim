428070838d5b76dd54721b472e0c87ddcce5fbc2
Marcel Arie <googlillo@gmail.com>
2021-06-15 18:13:33 +0200

    add: source

--- lua/telescope/_extensions/cheat/sources.lua
+++ lua/telescope/_extensions/cheat/sources.lua
@@ -1,49 +1,64 @@
-local p = require'plenary.path'
+local p = require 'plenary.path'
 local M = {}
 
-local parse  = function(path)
-  local content = p.readlines(path)
-  if content[1]:find('---', 1, true) then
-    local minus_count = 0
-    while minus_count < 2 do
-      if content[1]:find('---', 1, true) then minus_count = minus_count + 1 end
-      table.remove(content, 1)
+local parse = function(path)
+    local content = p.readlines(path)
+    if content[1]:find('---', 1, true) then
+        local minus_count = 0
+        while minus_count < 2 do
+            if content[1]:find('---', 1, true) then
+                minus_count = minus_count + 1
+            end
+            table.remove(content, 1)
+        end
     end
-  end
-  while content[1] == '' do
-    table.remove(content, 1)
-  end
+    while content[1] == '' do table.remove(content, 1) end
 
-  return table.concat(content, '\n')
+    return table.concat(content, '\n')
 end
 
-M[1] =  {
-  name = "cheatsheets",
-  uri = "https://github.com/cheat/cheatsheets",
-  ns = "unix",
-  root = "",
-  pattern = "",
-  ft = "sh",
-  parse = parse,
-  get_ns_keyword = function(path)
-    local name = vim.split(path, "/")
-    local a, b = unpack(vim.split(name[#name], "-"))
-    return a, b
-  end
+M[1] = {
+    name = "cheatsheets",
+    uri = "https://github.com/cheat/cheatsheets",
+    ns = "unix",
+    root = "",
+    pattern = "",
+    ft = "sh",
+    parse = parse,
+    get_ns_keyword = function(path)
+        local name = vim.split(path, "/")
+        local a, b = unpack(vim.split(name[#name], "-"))
+        return a, b
+    end
 }
 
 M[2] = {
-  name = "learnxinyminutes",
-  uri = "https://github.com/adambard/learnxinyminutes-docs",
-  root = "",
-  depth = 1,
-  pattern = ".*%.html%.markdown",
-  add_dirs = false,
-  ft = "markdown",
-  parse = parse,
-  get_ns_keyword = function(path)
-    return "lang", path:match('.*/([^./]+).*')
-  end
+    name = "learnxinyminutes",
+    uri = "https://github.com/adambard/learnxinyminutes-docs",
+    root = "",
+    depth = 1,
+    pattern = ".*%.html%.markdown",
+    add_dirs = false,
+    ft = "markdown",
+    parse = parse,
+    get_ns_keyword = function(path)
+        return "lang", path:match('.*/([^./]+).*')
+    end
+}
+
+M[3] = {
+    name = "cheatsheets",
+    uri = "",
+    ns = "unix",
+    root = "~/.config/cheat/cheatsheets/personal",
+    pattern = "",
+    ft = "sh",
+    parse = parse,
+    get_ns_keyword = function(path)
+        local name = vim.split(path, "/")
+        local a, b = unpack(vim.split(name[#name], "-"))
+        return a, b
+    end
 }
 
 return M
